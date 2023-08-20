865fb41a6bf5ec812cfb90caa330b8d6d8c7fba6
Neishun Lopati <91433068+wolfgangcrusader@users.noreply.github.com>
2023-08-20 18:20:26 +1200

    correction

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -9,12 +9,12 @@ public class CalcTest {
     @Test
     void testAddition() {
         assertEquals(4, c.add(2,2));
-        
+
     }
 
      @Test
     void testSubtraction() {
-      assertEquals(2, c.subtract(4,2));
+      assertEquals(8, c.subtract(4,2));
 
     }
 
